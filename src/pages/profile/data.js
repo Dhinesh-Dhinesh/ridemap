a00dcfbc9f895bca812c736735b3e97ad0fb4119
Dhinesh-Dhinesh <dhineshrandy953@gmail.com>
2022-11-12 22:11:40 +0530

    profile page routes and stops dropdown

---
+++ src/pages/profile/data.js
@@ -0,0 +1,32 @@
+export const routeData = [
+    {
+        id: 1,
+        route: 'CUDDALORE - Kanniya Kovil, Karikalampakkam, Uruvaiyar',
+        stops : ["Kanniya Kovil","Karikalampakkam","Uruvaiyar"]
+    },
+    {
+        id: 2,
+        route: 'KATTUKUPPAM - Ariyankuppam , Villiyanur',
+        stops : ["Ariyankuppam","Villiyanur"]
+    },
+    {
+        id: 3,
+        route: 'SEDARAPET - T.C X-Road, Jipmer, Iyyankuttipalayam',
+        stops : ["T.C X-Road","Jipmer","Iyyankuttipalayam"]
+    },
+    {
+        id: 4,
+        route: 'CUDDALORE - Kanniya Kovil, Karikalampakkam, Uruvaiyar',
+        stops : ["Kanniya Kovil","Karikalampakkam","Uruvaiyar"]
+    },
+    {
+        id: 5,
+        route: 'KATTUKUPPAM - Ariyankuppam , Villiyanur',
+        stops : ["Ariyankuppam","Villiyanur"]
+    },
+    {
+        id: 6,
+        route: 'SEDARAPET - T.C X-Road, Jipmer, Iyyankuttipalayam',
+        stops : ["T.C X-Road","Jipmer","Iyyankuttipalayam"]
+    }
+]
--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,22 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+//raw data
+import { routeData } from './data';
 
 export default function Profile() {
+
+    const [route, setRoute] = useState('Select your route');
+    const [isRouteDropDownOpen, setIsRouteDropDownOpen] = useState(false);
+    const [stopname,setStopname] = useState('Select your stop');
+    const [stop, setStop] = useState([]);
+    const [isStopDropDownOpen, setIsStopDropDownOpen] = useState(false);
+
+    function toggleRouteDropDown() {
+        setIsRouteDropDownOpen((prevState) => !prevState);
+    }
+
+    function toggleStopDropDown() {
+        setIsStopDropDownOpen((prevState) => !prevState);
+    }
+
     return (
-        <div className='flex flex-col text-white w-screen h-screen items-center bg-backgroundprimary py-5'>
-            <div className='bg-overlayprimary w-11/12 p-5 h-40 rounded-3xl shadow-2xl'>
-                <div className='flex'>
-                    <img src={require('./assets/user.png')} alt="profile" width={32} height={32}/>
-                    <p className='font-bold text-xl text-themeprimary ml-3 '>Profile</p>
+        <div className='flex flex-col w-screen h-screen items-center bg-backgroundprimary py-5'>
+            <h1 className='text-4xl font-bold text-gray-300 tracking-wider'>Profile</h1>
+            <img src={require('./assets/profile.png')} alt="profile" width={210} height={210} />
+            <h1 className='text-xl font-bold text-themeprimary -mt-5'>Dhinesh</h1>
+            <hr className='w-11/12 mt-2 border-gray-500' />
+            <div className='flex flex-col w-11/12 mt-5 '>
+                <div className='flex flex-row justify-start'>
+                    <h1 className='text-md font-bold text-gray-400'>Name : &nbsp;</h1>
+                    <h1 className='text-md font-bold text-gray-400'>Dhinesh</h1>
                 </div>
-                <p className="mt-3">Name : test</p>
-                <p className="mt-3">Email : test@gmail.com</p>
+                <div className='flex flex-row justify-start mt-5'>
+                    <h1 className='text-md font-bold text-gray-400'>Email &nbsp;: &nbsp;</h1>
+                    <h1 className='text-md font-bold text-gray-400'>dhinesh@gmail.com</h1>
+                </div>
             </div>
-            <div className='bg-overlayprimary w-11/12 p-5 h-40 rounded-3xl mt-5 shadow-2xl'>
-                <div className='flex'>
-                    <img src={require('./assets/route.png')} alt="route" width={32} height={32}/>
-                    <p className='font-bold text-xl text-themeprimary ml-3 '>Route</p>
+            <hr className='w-11/12 mt-2 border-gray-500' />
+            <div className='flex flex-col w-11/12 mt-5 '>
+                <div className='flex flex-row justify-start'>
+                    <h1 className='text-md font-bold text-gray-400'>Route : &nbsp;</h1>
+                    <button onClick={() => toggleRouteDropDown()}
+                        className="text-white w-52 bg-blue-500 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">{route}<svg className="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
                 </div>
-                <p className="mt-3">Select Your Route : Ariyur</p>
+                {/* Dropdown */}
+                <div id='scroll' className={`${isRouteDropDownOpen ? "" : "hidden"} ml-12 mt-16 z-10 w-64 bg-white rounded divide-y divide-gray-100 shadow dark:bg-overlayprimary absolute`}>
+                    <ul className="overflow-y-auto h-48 py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
+                        {
+                            routeData.map((item) => {
+                                return (
+                                    <li key={item.id}>
+                                        <p onClick={() => {
+                                            setRoute(item.route)
+                                            setStop(item.stops)
+                                            toggleRouteDropDown()
+                                        }} className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{item.route}</p>
+                                        <hr className='w-auto border-gray-500' />
+                                    </li>
+
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+                {
+                    route !== 'Select your route' && (
+                        <div className='flex flex-row justify-start mt-5'>
+                            <h1 className='text-md font-bold text-gray-400'>Stop <span className='pl-2.5'>:</span> &nbsp;</h1>
+                            <button onClick={() => toggleStopDropDown()}
+                                className="text-white w-52 bg-blue-500 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">{stopname}<svg className="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                        </div>
+                    )
+                }
+                {/* Dropdown */}
+                <div id='scroll' className={`${isStopDropDownOpen ? "" : "hidden"} ml-12 mt-36 z-10 w-64 bg-white rounded divide-y divide-gray-100 shadow dark:bg-overlayprimary absolute`}>
+                    <ul className="overflow-y-auto h-32 py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
+                        {
+                            stop && stop.map((item) => {
+                                return (
+                                    <li key={item}>
+                                        <p onClick={() => {
+                                            toggleStopDropDown()
+                                            setStopname(item)
+                                        }} className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{item}</p>
+                                        <hr className='w-auto border-gray-500' />
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         </div>
     )
