936037488959377da729211d18ca81473f2bad88
unknown <dhineshrandy953@gmail.com>
2022-11-16 14:35:55 +0530

    notification page

--- src/pages/notification/notification.js
+++ src/pages/notification/notification.js
@@ -2,8 +2,46 @@ import React from 'react';
 
 export default function Notification() {
     return (
-        <div className='flex w-screen h-screen justify-center items-center bg-backgroundprimary text-white'>
-            Notification
+        <div className='flex flex-col w-screen h-screen items-center bg-backgroundprimary text-white'>
+            <div className='bg-overlayprimary min-h-24 max-h-52 w-11/12 mt-5 p-2 rounded-xl shadow-2xl'>
+                <p className='text-themeprimary text-l font-bold'>Bus Reached to your nearest Stop!</p>
+                <p className='text-sm mt-3'>Bus no.25 reached to tindivanam be ready at your stop</p>
+            </div>
+            <div className='bg-overlayprimary max-h-24 w-11/12 mt-5 p-2 rounded-xl shadow-2xl'>
+                <p className='text-themeprimary text-l font-bold'>Bus Reached to your nearest Stop!</p>
+                <p className='text-sm mt-3'>Bus no.25 reached to tindivanam be ready at your stop</p>
+            </div>
+            <hr className='w-11/12 mt-4 border-gray-700' />
+            <div className='max-h-24 w-11/12 mt-1 pt-1 ml-2 text-gray-400 text-sm'>
+                yesterday 10:00PM
+            </div>
+            <div className='bg-overlayprimary max-h-24 w-11/12 mt-5 p-2 rounded-xl shadow-2xl'>
+                <p className='text-themeprimary text-l font-bold'>Bus Reached to your nearest Stop!</p>
+                <p className='text-sm mt-3'>Bus no.25 reached to tindivanam be ready at your stop</p>
+            </div>
+            <div className='bg-overlayprimary max-h-24 w-11/12 mt-5 p-2 rounded-xl shadow-2xl'>
+                <p className='text-themeprimary text-l font-bold'>Bus Reached to your nearest Stop!</p>
+                <p className='text-sm mt-3'>Bus no.25 reached to tindivanam be ready at your stop</p>
+            </div>
+            <div className='bg-overlayprimary max-h-24 w-11/12 mt-5 p-2 rounded-xl shadow-2xl'>
+                <p className='text-themeprimary text-l font-bold'>Bus Reached to your nearest Stop!</p>
+                <p className='text-sm mt-3'>Bus no.25 reached to tindivanam be ready at your stop</p>
+            </div>
+            <div className='bg-overlayprimary max-h-24 w-11/12 mt-5 p-2 rounded-xl shadow-2xl'>
+                <p className='text-themeprimary text-l font-bold'>Bus Reached to your nearest Stop!</p>
+                <p className='text-sm mt-3'>Bus no.25 reached to tindivanam be ready at your stop</p>
+            </div>
+            <div className='bg-overlayprimary max-h-24 w-11/12 mt-5 p-2 rounded-xl shadow-2xl'>
+                <p className='text-themeprimary text-l font-bold'>Bus Reached to your nearest Stop!</p>
+                <p className='text-sm mt-3'>Bus no.25 reached to tindivanam be ready at your stop</p>
+            </div>
+            {/* clear all button */}
+            <div className='bg-overlayprimary p-2 px-5 mt-5 rounded-2xl font-bold text-gray-400 hover:text-gray-100'>
+                Clear all
+            </div>
+            <div className='mt-16'>
+                &nbsp;
+            </div>
         </div>
     )
 }
--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -135,7 +135,7 @@ export default function Profile() {
                     <label className="inline-flex relative items-center cursor-pointer -ml-20">
                         <input type="checkbox" value="" id="purple-toggle" className="sr-only peer" defaultChecked onChange={() => toggleNotification()} />
                         <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-2 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
-                        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{isNotificationOn ? "On" : "Off"}</span>
+                        <span className="ml-3 text-sm font-medium text-gray-400">{isNotificationOn ? "On" : "Off"}</span>
                     </label>
                 </div>
                 <div className='grid grid-cols-3 justify-items-start w-80 mt-5'>
@@ -148,7 +148,7 @@ export default function Profile() {
                             setThemeChecked(prevState => !prevState);
                         }} />
                         <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-2 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
-                        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{isThemeChecked ? "Dark" : "Lite"}</span>
+                        <span className="ml-3 text-sm font-medium text-gray-400">{isThemeChecked ? "Dark" : "Lite"}</span>
                     </label>
                 </div>
             </div>
