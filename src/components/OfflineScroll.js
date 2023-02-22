8b47eb92466c28d6bdf5bbecc961f12001028a5a
Dhinesh-Dhinesh <dhineshrandy953@gmail.com>
2023-02-22 09:41:29 +0530

    offlline ui

--- src/components/OfflineScroll.js
+++ src/components/OfflineScroll.js
@@ -8,19 +8,22 @@ const defaultOptionsLottie = {
     autoplay: true,
     animationData: OfflineAnimation,
     renderer: 'svg'
-  }
+}
 
-export default function OfflineScroll({busno}) {
+export default function OfflineScroll({ busno }) {
     return (
         <div className="m-4 h-32 w-52 rounded-2xl bg-backgroundprimary">
-            <div className="relative top-2 m-2 flex h- flex-col rounded-sm border-l-4 p-2">
+            <div className="relative top-2 m-2 flex h- flex-col rounded-sm border-l-4 px-2 border-red-600">
                 <div className="font-bold text-white">Bus No {busno}</div>
                 <div className="mt-4 font-bold text-white flex">
                     <p>Device Offline</p>
                     <Lottie options={defaultOptionsLottie} height={30} width={30} isClickToPauseDisabled={true} />
                 </div>
             </div>
+            <div className="border flex justify-center items-center m-3 rounded-lg cursor-pointer
+            text-sm font-bold h-8 mt-4 text-slate-600">
+                Show Map
+            </div>
         </div>
-
     )
 }
